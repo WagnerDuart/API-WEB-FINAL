0a07f2b36688deb7cd5425d40201a26278b5bd0a
WagnerDuart <124386335+WagnerDuart@users.noreply.github.com>
2024-02-26 21:54:34 -0300

    Update Employee.js

--- models/Employee.js
+++ models/Employee.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 
 const EmployeeSchema = new Schema({
     name: {type: String, require: true},
+    midia: {type: String, require: true},
     training: {type: String, require: true},
     description: {type: String, require: true},
     social_media: {type: String, require: true},
